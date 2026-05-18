a50a454ba5e466150f2fca52791d97f0d51b97a3
Macchi-Federico <federicomaurilio.macchi@istitutoagnelli.it>
2026-05-18 09:35:32 +0200

    fix prerender warnings

--- svelte.config.js
+++ svelte.config.js
@@ -2,16 +2,20 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	compilerOptions: {
-		runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
-	},
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
-	}
+        compilerOptions: {
+                runes: ({ filename }) => (filename.split(/[/\\]/).includes('node_modules') ? undefined : true)
+        },
+        kit: {
+                adapter: adapter({
+                        pages: 'build',
+                        assets: 'build',
+                        fallback: null
+                }),
+                prerender: {
+                        handleHttpError: 'warn',
+                        handleMissingId: 'warn'
+                }
+        }
 };
 
 export default config;
